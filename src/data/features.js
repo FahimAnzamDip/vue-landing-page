4758f1173664c3f659fbc206a93a708b0eaf98c6
Fahim Anzam Dip <dfahimanzam@gmail.com>
2023-07-18 19:15:21 +0600

    First Build

--- src/data/features.js
+++ src/data/features.js
@@ -3,7 +3,7 @@ const features = [
         id: 1,
         name: "Simple Bookmaring",
         details: {
-            imageUrl: "/images/illustration-features-tab-1.svg",
+            imageUrl: "images/illustration-features-tab-1.svg",
             title: "Bookmark in one click",
             description: "Organize your bookmarks however you like. Our simple drag-n-drop interface gives you complete control over how you manage your favorite websites.",
             link: "#",
@@ -13,7 +13,7 @@ const features = [
         id: 2,
         name: "Speedy Searching",
         details: {
-            imageUrl: "/images/illustration-features-tab-2.svg",
+            imageUrl: "images/illustration-features-tab-2.svg",
             title: "Intelligent search",
             description: "Get quick results with our intelligent search feature. Find your bookmarks effortlessly and save time navigating through your favorite websites.",
             link: "#",
@@ -23,7 +23,7 @@ const features = [
         id: 3,
         name: "Speedy Sharing",
         details: {
-            imageUrl: "/images/illustration-features-tab-3.svg",
+            imageUrl: "images/illustration-features-tab-3.svg",
             title: "Easy sharing",
             description: "Share your bookmarks seamlessly with others. Our intuitive sharing options allow you to collaborate and spread the word about your favorite websites.",
             link: "#",
